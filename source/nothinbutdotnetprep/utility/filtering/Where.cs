01f50a045e0fd3006c0e1db31dbe514016d2f98e
peng li <ilgnep@hotmail.com>
2011-08-08 16:10:39 -0600

    Pushing new changes

--- source/nothinbutdotnetprep/utility/filtering/Where.cs
+++ source/nothinbutdotnetprep/utility/filtering/Where.cs
@@ -1,12 +1,28 @@
 using System;
+using nothinbutdotnetprep.collections;
 
 namespace nothinbutdotnetprep.utility.filtering
 {
     public class Where<ItemToMatch>
     {
-        public static Something has_a<PropertyType>(Func<ItemToMatch, PropertyType> accessor)
+        public static Filter<ItemToMatch, PropertyType> has_a<PropertyType>(Func<ItemToMatch, PropertyType> accessor)
         {
-            throw new NotImplementedException();
+            return new Filter<ItemToMatch, PropertyType>(accessor);
+        }
+    }
+
+    public class Filter<ItemToMatch, PropertyType>
+    {
+        private readonly Func<ItemToMatch, PropertyType> accessor;
+
+        public Filter(Func<ItemToMatch, PropertyType> accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public Condition<Movie> equal_to(ProductionStudio studio)
+        {
+            return accessor(studio).Equals();
         }
     }
 }
